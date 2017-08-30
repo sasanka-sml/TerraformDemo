0827820ee2e720b796ac5e0b818d569ff6f32622
sasanka-sml <sasanka.sml@gmail.com>
2017-08-30 09:28:07 +0530

    newly Added

---
+++ vars.tf
@@ -0,0 +1,25 @@
+variable "AWS_ACCESS_KEY" {}
+
+
+variable "AWS_SECRET_KEY" {}
+
+
+variable "AWS_REGION" {
+
+  default = "us-east-1"
+
+}
+
+
+variable "AMIS" {
+
+  type = "map"
+
+
+  default = {
+
+    us-east-1 = "ami-a4c7edb2"
+
+  }
+
+}
